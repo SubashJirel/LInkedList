008b855b82acdf797792ce35b3fdc430479002e7
Subash Jirel <subashjrl@gmail.com>
2023-09-29 11:27:05 +0545

    initialized files

---
+++ createNode.js
@@ -0,0 +1,8 @@
+class createNode {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next == next;
+  }
+}
+
+export default createNode;
---
+++ linkedList.js
@@ -0,0 +1,15 @@
+import createNode from './createNode';
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  //Insert first node
+
+  //insert last node
+
+  //get at index
+
+  //remove at index
+}
